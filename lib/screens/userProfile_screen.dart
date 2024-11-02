3024fe13bf08ce91a51aac67afdcefeb404dac35
Your Name <sultankocagoz.448@gmail.com>
2024-11-02 11:40:22 +0300

    Takip durumu eklendi

--- lib/screens/userProfile_screen.dart
+++ lib/screens/userProfile_screen.dart
@@ -16,12 +16,59 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   List<Post> _posts = [];
-  String? userName; // Kullanıcı adını burada saklayacağız
+  String? userName;
+  bool isFollowing = false;
 
   Future<Map<String, dynamic>?> _getUserProfile() async {
     DocumentSnapshot userDoc =
         await _firestore.collection('users').doc(widget.userId).get();
     return userDoc.data() as Map<String, dynamic>?; // Profil verilerini al
+  }
+
+  Future<void> _checkIfFollowing() async {
+    final currentUser = _auth.currentUser;
+    if (currentUser == null) return;
+
+    DocumentSnapshot followingDoc = await _firestore
+        .collection('users')
+        .doc(currentUser.uid)
+        .collection('following')
+        .doc(widget.userId)
+        .get();
+
+    setState(() {
+      isFollowing = followingDoc.exists; // Takip ediliyor mu kontrol et
+    });
+  }
+
+  Future<void> _toggleFollow() async {
+    final currentUser = _auth.currentUser;
+    if (currentUser == null) return;
+
+    final followingRef = _firestore
+        .collection('users')
+        .doc(currentUser.uid)
+        .collection('following')
+        .doc(widget.userId);
+
+    if (isFollowing) {
+      // Takipten çık
+      await followingRef.delete();
+    } else {
+      // Takip et
+      await followingRef.set({});
+    }
+
+    // Takip durumunu güncelle
+    setState(() {
+      isFollowing = !isFollowing;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIfFollowing();
   }
 
   @override
@@ -32,20 +79,19 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: FutureBuilder<Map<String, dynamic>?>(
-            // FutureBuilder ile kullanıcı adını al
             future: _getUserProfile(),
             builder: (context, snapshot) {
               if (!snapshot.hasData) {
                 return const Text('Yükleniyor...');
               }
               final userData = snapshot.data!;
-              userName = userData['username'] ?? 'Ad'; // Kullanıcı adını sakla
+              userName = userData['username'] ?? 'Ad';
               return Text(
                 userName!,
                 style: const TextStyle(
                   fontWeight: FontWeight.bold,
                 ),
-              ); // Kullanıcı adını göster
+              );
             },
           ),
           leading: IconButton(
@@ -55,7 +101,6 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               icon: const Icon(Icons.arrow_back)),
         ),
         body: FutureBuilder<Map<String, dynamic>?>(
-          // Kullanıcı profil bilgilerini almak için tekrar FutureBuilder kullan
           future: _getUserProfile(),
           builder: (context, snapshot) {
             if (!snapshot.hasData) {
@@ -122,177 +167,177 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                       ))),
                       height: MediaQuery.of(context).size.height / 9,
                       child: Row(
-                        mainAxisAlignment:
-                            MainAxisAlignment.spaceAround, // Butonları yay
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
                           ElevatedButton(
                             style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
+                              backgroundColor:
+                                  isFollowing ? Colors.white : Colors.blue,
                               shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                    10), // Kenar yuvarlama
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                              elevation: 5, // Gölge efekti
-
+                              elevation: 5,
                               fixedSize: Size(
                                   MediaQuery.of(context).size.width / 2.5,
                                   MediaQuery.of(context).size.height / 18),
                             ),
                             onPressed: () {
-                              // Takip et butonuna tıklandığında yapılacak işlemler
+                              _toggleFollow();
                             },
-                            child: const Text(
-                              "Takip et",
+                            child: Text(
+                              isFollowing ? "Takip" : "Takip Et",
                               style: TextStyle(
-                                color: Colors.black, // Buton metni rengi
+                                color:
+                                    isFollowing ? Colors.black : Colors.white,
                                 fontSize: 15,
                               ),
                             ),
                           ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                    10), // Kenar yuvarlama
+                          if (isFollowing) ...[
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                elevation: 5,
+                                fixedSize: Size(
+                                    MediaQuery.of(context).size.width / 2.5,
+                                    MediaQuery.of(context).size.height / 18),
                               ),
-                              elevation: 5, // Gölge efekti
-
-                              fixedSize: Size(
-                                  MediaQuery.of(context).size.width / 2.5,
-                                  MediaQuery.of(context).size.height / 18),
-                            ),
-                            onPressed: () {
-                              // Takip et butonuna tıklandığında yapılacak işlemler
-                            },
-                            child: const Text(
-                              "Mesaj",
-                              style: TextStyle(
-                                color: Colors.black, // Buton metni rengi
-                                fontSize: 15,
+                              onPressed: () {
+                                // Mesaj gönder butonuna tıklandığında yapılacak işlemler
+                              },
+                              child: const Text(
+                                "Mesaj",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                ),
                               ),
                             ),
-                          ),
+                          ],
                         ],
                       ),
                     ),
                   ),
 
                   // Kullanıcının gönderileri
-                  StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance
-                        .collection('posts')
-                        .where('userId',
-                            isEqualTo: widget
-                                .userId) // Başka bir kullanıcının gönderilerini al
-                        .snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
+                  if (isFollowing)
+                    StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance
+                          .collection('posts')
+                          .where('userId', isEqualTo: widget.userId)
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
 
-                      _posts = snapshot.data!.docs.map((doc) {
-                        return Post.fromMap(
-                            doc.id, doc.data() as Map<String, dynamic>);
-                      }).toList();
+                        _posts = snapshot.data!.docs.map((doc) {
+                          return Post.fromMap(
+                              doc.id, doc.data() as Map<String, dynamic>);
+                        }).toList();
 
-                      if (_posts.isEmpty) {
-                        return const Center(
-                          child: Text("Henüz Gönderi Yok"),
-                        );
-                      }
+                        if (_posts.isEmpty) {
+                          return const Center(
+                            child: Text("Henüz Gönderi Yok"),
+                          );
+                        }
 
-                      return ListView.builder(
-                        itemCount: _posts.length,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          final post = _posts[index];
+                        return ListView.builder(
+                          itemCount: _posts.length,
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            final post = _posts[index];
 
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Container(
-                              color: Colors.transparent,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 8.0, vertical: 8.0),
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Container(
-                                          decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            border: Border.all(
-                                                color: Colors.black,
-                                                width: 3.0),
-                                          ),
-                                          child: CircleAvatar(
-                                              radius: 18,
-                                              backgroundColor: Colors.grey[300],
-                                              backgroundImage: profileImage !=
-                                                          null &&
-                                                      profileImage.isNotEmpty
-                                                  ? NetworkImage(profileImage)
-                                                  : const AssetImage(
-                                                          "lib/assets/images/avatar.png")
-                                                      as ImageProvider<
-                                                          Object>?),
-                                        ),
-                                        const SizedBox(width: 8),
-                                        Text(
-                                          userName ??
-                                              'Anonim', // Kullanıcı adını göster
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  if (post.imagePath != null)
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Container(
+                                color: Colors.transparent,
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
                                     Padding(
-                                      padding:
-                                          const EdgeInsets.only(bottom: 12.0),
-                                      child: Image.network(
-                                        post.imagePath!,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  Padding(
-                                    padding: const EdgeInsets.fromLTRB(
-                                        8.0, 2.0, 0.0, 10.0),
-                                    child: RichText(
-                                      text: TextSpan(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 8.0, vertical: 8.0),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
                                         children: [
-                                          TextSpan(
-                                            text: "${userName ?? 'Ad'}  ",
-                                            style: const TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.black),
+                                          Container(
+                                            decoration: BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              border: Border.all(
+                                                  color: Colors.black,
+                                                  width: 3.0),
+                                            ),
+                                            child: CircleAvatar(
+                                                radius: 18,
+                                                backgroundColor:
+                                                    Colors.grey[300],
+                                                backgroundImage: profileImage !=
+                                                            null &&
+                                                        profileImage.isNotEmpty
+                                                    ? NetworkImage(profileImage)
+                                                    : const AssetImage(
+                                                            "lib/assets/images/avatar.png")
+                                                        as ImageProvider<
+                                                            Object>?),
                                           ),
-                                          TextSpan(
-                                            text: post.content,
+                                          const SizedBox(width: 8),
+                                          Text(
+                                            userName ?? 'Anonim',
                                             style: const TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.black),
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                            ),
                                           ),
                                         ],
                                       ),
                                     ),
-                                  ),
-                                ],
+                                    if (post.imagePath != null)
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 12.0),
+                                        child: Image.network(
+                                          post.imagePath!,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          8.0, 2.0, 0.0, 10.0),
+                                      child: RichText(
+                                        text: TextSpan(
+                                          children: [
+                                            TextSpan(
+                                              text: "${userName ?? 'Ad'}  ",
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black),
+                                            ),
+                                            TextSpan(
+                                              text: post.content,
+                                              style: const TextStyle(
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  ),
+                            );
+                          },
+                        );
+                      },
+                    ),
                 ],
               ),
             );
